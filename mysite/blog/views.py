f134d7a6dbd67f4318fbc9642acee90c34610a32
YM Choi <archoiym@gmail.com>
2017-02-08 12:13:54 +0900

    파트3에 2번째 완료

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,3 +7,9 @@ def post_list(request):
     post_list = Post.objects.all().order_by("-published_date")
     context = {"post_list": post_list}
     return render(request, "blog/post-list.html", context)
+
+def post_detail(request, post_id):
+    p = Post.objects.get(id=post_id)
+    context = {"p": p}
+    return render(request, "blog/post-detail.html", context)
+
